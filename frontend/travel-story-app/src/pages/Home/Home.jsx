06edc227704bd58b392911f5ad101d57fedf1b97
AnkitPrajapat1 <141557711+AnkitPrajapat1@users.noreply.github.com>
2025-04-18 18:06:08 +0530

    Update Home.jsx

--- frontend/travel-story-app/src/pages/Home/Home.jsx
+++ frontend/travel-story-app/src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import { useNavigate } from "react-router-dom";
-import axiosInstance from "../../utils/axiosinstance";
+import axiosInstance from "../../utils/axiosInstance";
 import { MdAdd } from "react-icons/md";
 import Modal from "react-modal";
 import AddEditTravelStory from "./AddEditTravelStory";
