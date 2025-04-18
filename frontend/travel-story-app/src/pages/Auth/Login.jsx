686df835e1d368e75d7208a22a9620eb74d0199b
AnkitPrajapat1 <141557711+AnkitPrajapat1@users.noreply.github.com>
2025-04-18 18:05:12 +0530

    Update Login.jsx

--- frontend/travel-story-app/src/pages/Auth/Login.jsx
+++ frontend/travel-story-app/src/pages/Auth/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PasswordInput from "../../components/Input/PasswordInput";
 import { useNavigate } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
-import axiosInstance from "../../utils/axiosinstance";
+import axiosInstance from "../../utils/axiosInstance";
 
 const Login = () => {
   const [email, setEmail] = useState("");
